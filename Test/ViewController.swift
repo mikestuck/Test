681fac37eaf34596219d42ee8a35d22b9288477a
Apple Builder <applebuilder@slyces-macbook-pro-3.local>
2016-09-01 16:34:29 -0700

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Apple Builder on 9/1/16.
+//  Copyright © 2016 Apple Builder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
